b33ce0e16a02d19ece74d80930279b8595c837a7
despair3 <nospam@>
2024-07-17 23:02:09 +0500

    quantile 75 \ 25 * 100%

--- testing/append_short.py
+++ testing/append_short.py
@@ -6,13 +6,15 @@ def ashot(df):
     data = {init.ticker_column: [],
             init.date_column: [],
             "mean": [],
-            "std_m": []}
+            "std_m": [],
+            "q25%": []}
     for i in df:
-        t,d,m,s = short_data(i)
+        t,d,m,s, q = short_data(i)
         data[init.ticker_column].append(t)
         data[init.date_column].append(d)
         data["mean"].append(m)
         data["std_m"].append(s)
+        data["q25%"].append(q)
     df = pd.DataFrame(data)
     pd.options.display.float_format = '{:.3f}'.format
     print(df)
--- testing/short_report.py
+++ testing/short_report.py
@@ -9,5 +9,8 @@ def short_data(df :pd.DataFrame):
     start_date = df[init.date_column].iat[0]
     mean_open = df[init.open_column].mean()
     std_open = df[init.open_column].std()
+    q25 = df[init.high_column].quantile(0.75) / \
+          df[init.low_collumn].quantile(0.25)
+    q25 = (q25-1) * 100
     # print(ticker, start_date, mean_open, std_open)
-    return ticker, start_date, mean_open, std_open
+    return ticker, start_date, mean_open, std_open, q25
