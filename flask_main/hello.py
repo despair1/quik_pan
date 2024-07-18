3424fdd2ba0b648c20d5325de75cbaeb17822fe3
despair3 <nospam@>
2024-07-19 03:37:12 +0500

    select lists read text from filenames in ofz folder

--- flask_main/hello.py
+++ flask_main/hello.py
@@ -5,26 +5,7 @@ import sys
 sys.path.insert(1, "testing")
 
 app = Flask(__name__, static_url_path='/static')
-data = {'Name': ['Alice', 'Bob', 'Charlie'],
-        'Age': [25, 30, 35]}
-df = pd.DataFrame(data)
-from short_report import short_report
-from split_df import split_df
-from delta_days import delta_days
-from mean_diff import mean_diff
 
-df = pd.read_csv('../ОФЗ 26243 [Price]''.txt')
-
-# print(df.dtypes)
-# df = new_time(df)
-df = split_df(df)
-# for i in df:
-#     print(i.describe())
-# describe(df)
-# t = short_data(df)
-df = short_report(df)
-df = delta_days(df)
-df = mean_diff(df)
 l = """
 <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='style.css') }}">
 """
@@ -35,6 +16,8 @@ from first_report import first_report
 def hello_world():
     # return l + "<p>Hello, World!</p>" + df.to_html(border=3)
     df_all, filenames, str_select = file_list()
+    print(str_select)
     df1 = first_report(df_all[0])
     df2 = first_report(df_all[1])
-    return render_template('2df.html', df1=df1, df2=df2)
+    return render_template('2df.html', df1=df1, df2=df2,
+                           str_select=str_select)
