714d5c1ce959dc85c92a68824682bad072ef1431
despair3 <nospam@>
2024-07-19 05:48:49 +0500

    show 2 ofz in form page

--- flask_main/postOfz.py
+++ flask_main/postOfz.py
@@ -1,6 +1,11 @@
-from flask import request
+from flask import request, render_template
 from file_list import file_list
+from first_report import first_report
 
 def test():
     df_all, filenames, str_select = file_list()
+    df1 = first_report(df_all[int(request.form["selectLeft"])])
+    df2 = first_report(df_all[int(request.form["selectRight"])])
+    return render_template('2df.html', df1=df1, df2=df2,
+                           str_select=str_select)
     return 'it works! '+filenames[int(request.form["selectLeft"])]
