3669b732091a070a14effb67d61ba436a5bceef7
despair3 <nospam@>
2024-07-19 18:15:03 +0500

    default start day

--- flask_main/hello.py
+++ flask_main/hello.py
@@ -1,26 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, session
 import pandas as pd
 import sys
 
 sys.path.insert(1, "testing")
 
 app = Flask(__name__, static_url_path='/static')
+app.secret_key = 'your_secret_key_here'
 
-l = """
-<link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='style.css') }}">
-"""
 from file_list import file_list
 from first_report import first_report
 
 @app.route("/")
 def hello_world():
     # return l + "<p>Hello, World!</p>" + df.to_html(border=3)
+    if "start_day" not in session:
+        session["start_day"] = 20231004
     df_all, filenames, str_select = file_list()
     # print(str_select)
     df1 = first_report(df_all[0])
     df2 = first_report(df_all[1])
+
     return render_template('2df.html', df1=df1, df2=df2,
-                           str_select=str_select)
+                           str_select=str_select, default_start_data=session["start_day"])
 
 import postOfz
 app.add_url_rule('/process_form', methods=['GET', 'POST'], view_func=postOfz.test)
--- flask_main/postOfz.py
+++ flask_main/postOfz.py
@@ -1,4 +1,4 @@
-from flask import request, render_template
+from flask import request, render_template, session
 from file_list import file_list
 from first_report import first_report
 
@@ -8,5 +8,5 @@ def test():
     df1 = first_report(df_all[int(request.form["selectLeft"])])
     df2 = first_report(df_all[int(request.form["selectRight"])])
     return render_template('2df.html', df1=df1, df2=df2,
-                           str_select=str_select)
+                           str_select=str_select, default_start_data=session["start_day"])
     return 'it works! '+filenames[int(request.form["selectLeft"])]
